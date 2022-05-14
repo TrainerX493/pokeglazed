c50e91ac615b637c5b1872547669bff1de683111
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-05-15 00:09:58 +0200

    reverted wrongful commit

--- src/pokedex.c
+++ src/pokedex.c
@@ -4733,7 +4733,7 @@ static u32 GetPokedexMonPersonality(u16 species)
 
 u16 CreateMonSpriteFromNationalDexNumber(u16 nationalNum, s16 x, s16 y, u16 paletteSlot)
 {
-    nationalNum = NationalPokedexNumToSpeciesHGSS(nationalNum);
+    nationalNum = NationalPokedexNumToSpecies(nationalNum);
     return CreateMonPicSprite_HandleDeoxys(nationalNum, SHINY_ODDS, GetPokedexMonPersonality(nationalNum), TRUE, x, y, paletteSlot, TAG_NONE);
 }
 
