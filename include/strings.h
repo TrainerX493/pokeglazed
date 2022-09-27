4a3b91ae3690a14f630ba43a91627f8fc45cc6a7
HunarPG <92902941+HunarPG@users.noreply.github.com>
2022-09-27 13:08:41 +0530

    LGPE-style Premier Balls

--- include/strings.h
+++ include/strings.h
@@ -3049,4 +3049,6 @@ extern const u8 gText_MoveItemWhere[];
 extern const u8 gText_XsYAnd[];
 extern const u8 gText_XsYWereSwapped[];
 
+extern const u8 gText_ThrowInPremierBalls[];
+
 #endif // GUARD_STRINGS_H
