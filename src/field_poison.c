f4253f246941189fbdce40b92fd867e46f411149
HunarPG <92902941+HunarPG@users.noreply.github.com>
2022-09-27 13:02:20 +0530

    Making Pokémon with Poison Heal be unaffected by poison in the overworld

--- src/field_poison.c
+++ src/field_poison.c
@@ -16,6 +16,7 @@
 #include "trainer_hill.h"
 #include "constants/field_poison.h"
 #include "constants/party_menu.h"
+#include "constants/abilities.h"
 
 static bool32 IsMonValidSpecies(struct Pokemon *pokemon)
 {
@@ -122,7 +123,7 @@ s32 DoPoisonFieldEffect(void)
 
     for (i = 0; i < PARTY_SIZE; i++)
     {
-        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
+        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN&& GetMonAbility(&gPlayerParty[i]) != ABILITY_POISON_HEAL)
         {
             // Apply poison damage
             hp = GetMonData(pokemon, MON_DATA_HP);
